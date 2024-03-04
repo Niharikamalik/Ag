2a5010ab6c3e1b96688f71ecff6b8ee8df41ab2d
Niharika Malik <niharikamalik50@gmail.com>
2024-03-04 14:48:50 +0530

    column

---
+++ src/app/app.module.ts
@@ -0,0 +1,15 @@
+import { AgGridComponent } from "./../../../agGrid/src/app/pages/ag-grid/ag-grid.component";
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { AgGridAngular } from "ag-grid-angular";
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { HttpClient, HttpClientModule } from "@angular/common/http";
+
+@NgModule({
+  declarations: [AppComponent, AgGridComponent],
+  imports: [BrowserModule, AppRoutingModule, AgGridAngular, HttpClientModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
